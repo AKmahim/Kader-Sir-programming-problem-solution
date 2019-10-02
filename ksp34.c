41eb645d1dea01e965bf1e2c88462876e1113171
HKmahim <mdmahimhossen134@gmail.com>
2019-10-03 02:25:16 +0600

    New code

--- ksp34.c
+++ ksp34.c
@@ -8,6 +8,17 @@
 #include<stdio.h>
 int  main()
 {
+    FILE *fp1,*fp2;
+    fp1 = fopen("main_file.txt","a");
+    fp2 = fopen("append_file.txt","r");
+
+    char line[1000];
+    fgets(line,1000,fp2);
+    fputs(line,fp1);
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
 
 
 }
